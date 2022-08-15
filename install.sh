f09f730e3840441a5a0067b35816c50f3c013621
Kevin Barton <kbarton@tamusa.edu>
2022-08-15 10:41:48 -0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Install nmap, net-tools, bridge-utils, docker and containerlab on Ubuntu
+
+# Install prerequisites
+sudo apt install apt-transport-https ca-certificates bridge-utils
+sudo apt install -y curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install -y \
+  docker-ce \
+  nmap \
+  net-tools \
+  bridge-utils
+
+# Install containerlab
+sudo bash -c "$(curl -sL https://get-clab.srlinux.dev)"
