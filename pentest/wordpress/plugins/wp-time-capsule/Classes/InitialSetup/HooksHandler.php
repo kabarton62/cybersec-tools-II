82eb60b51ce6dc801069f620f8017bb2bd62a8e1
Kevin Barton <kbarton@tamusa.edu>
2022-11-17 22:35:31 -0700

    Delete pentest directory

--- pentest/wordpress/plugins/wp-time-capsule/Classes/InitialSetup/HooksHandler.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Wptc_InitialSetup_Hooks_Handler extends Wptc_Base_Hooks_Handler {
-	protected $settings;
-
-	public function __construct() {
-		$this->settings = WPTC_Base_Factory::get('Wptc_InitialSetup');
-	}
-}
--- pentest/wordpress/plugins/wp-time-capsule/Pro/ProFactory.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class WPTC_Pro_Factory extends WPTC_Base_Factory {
-	private function __construct() {
-
-	}
-}
